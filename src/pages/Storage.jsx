0e8b6d9b4395e3404fa455c6200de294d0b4ce4b
Lordxavi97 <jaaca97@gmail.com>
2025-02-28 23:45:54 +0100

    Update Storage.jsx

--- src/pages/Storage.jsx
+++ src/pages/Storage.jsx
@@ -4,30 +4,28 @@ import usePokemonSearch from "../hooks/usePokemonSearch";
 const Storage = ({}) => {
     const [query, setQuery] = useState("");
     const { results, loading } = usePokemonSearch(query);
+    const [creature, setCreature] = useState([]);
 
     return (
         <>
-            <div className="bg-blue-500">
-                Hola este es el almacen
+            <div className="p-4 bg-amber-200">
+                <h2 className="text-xl font-bold">Introducir criatura</h2>
+                <input
+                    type="text"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    placeholder="Escribe un nombre..."
+                    className="border p-2 rounded w-full"
+                />
+                {loading && <p className="text-gray-500">Cargando...</p>}
+                <ul className="border rounded mt-2 max-h-40 overflow-y-auto">
+                    {results.map((name) => (
+                        <li key={name} className="p-2 hover:bg-gray-200 cursor-pointer" onClick={console.log(this.value)}>
+                            {name}
+                        </li>
+                    ))}
+                </ul>
             </div>
-            <div className="p-4">
-            <h2 className="text-xl font-bold">Buscar Pokémon</h2>
-            <input
-                type="text"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                placeholder="Escribe un nombre..."
-                className="border p-2 rounded w-full"
-            />
-            {loading && <p className="text-gray-500">Cargando...</p>}
-            <ul className="border rounded mt-2 max-h-40 overflow-y-auto">
-                {results.map((name) => (
-                    <li key={name} className="p-2 hover:bg-gray-200 cursor-pointer">
-                        {name}
-                    </li>
-                ))}
-            </ul>
-        </div>
         </>
     )
 }
