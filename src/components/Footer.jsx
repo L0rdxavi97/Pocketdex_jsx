3e8b7732695f76e0ef874b9221a6e2047997201b
Lordxavi97 <jaaca97@gmail.com>
2025-02-28 14:42:05 +0100

    reintent

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+
+
+export default function Footer(){
+    return (
+        <footer className="bg-stone-800 flex flex-col text-white text-sm ">
+            <div className="flex flex-row justify-center gap-2">
+                <h4>Aviso Legal</h4>
+                <h4>Politica de Privacidad</h4>
+                <h4>Politica de Cookies</h4>
+                <h4>Canal de Denuncias</h4>
+            </div>
+            <div className="flex flex-col text-center my-3">
+                <h4>Este sitio web utiliza cookies para mejorar tu experiencia de usuario. Al continuar navegando, aceptas nuestra Política de Cookies.
+                </h4>
+                <p>Copyright ©PocketDex 2024</p>
+            </div>
+            <div className="flex flex-row justify-center">
+                <a href="https://x.com/javieraxedo"><img src="/img/rec/twitter.png"  alt="imagen twitter"/></a>
+                <a href="https://www.instagram.com/lordxavi97"><img src="/img/rec/instagram.png" alt="imagen instagram"/></a>
+                <a href="https://www.linkedin.com/in/javier-acedo-caballero-5707562b8/"><img src="/img/rec/linkedin.png" alt="imagen linkedin"/></a>
+                <a href="https://github.com/L0rdxavi97"><img src="/img/rec/github.png"  alt="imagen github"/></a>
+            </div>
+        </footer>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import ChisteComp from '../components/ChisteComp';
+
+const Home = ({chiste, ContarChiste}) => {
+  return (
+    <>
+    <main className="flex flex-col justify-center text-center bg-amber-200 pt-3 gap-8 pb-8">
+        <h1 className='text-5xl font-bold'>Bienvenido a PocketDex</h1>
+        <div>
+            <img className='justify-self-center h-40 hover:-translate-y-1 hover:scale-130 transition delay-150 duration-300 ease-in-out' src="/img/rec/tortank-pokémon.gif" alt="imagen blastoise shiny" id="red"/>
+        </div>
+        <div>
+            <h2 className='font-semibold text-2xl'>Esta pagina consiste en un portal de gestion de tus criaturas de bolsillo.</h2>
+            <h2 className='font-semibold text-2xl'>Este portal tiene las secciones de:</h2>
+            <ul className='font-semibold text-2xl underline leading-10 mb-4'>
+                <li><h3>Almacenamiento</h3></li>
+                <li><h3>Foro</h3></li>
+                <li><h3>Otros Usuarios</h3></li>
+                <li><h3>Perfil</h3></li>
+            </ul>
+            
+        </div>
+        <div className='bg-amber-100 lg:m-auto m-8 rounded-2xl p-3'>
+            <h2 className='font-bold text-3xl underline'>Almacenamiento</h2>
+            <h3 className='font-semibold my-5'>Seccion que permite la gestion de las criaturas de cada uno. 
+                Esta seccion nos permite la introduccion, visualizacion, modificacion y la eliminacion de las criaturas.</h3>
+                <div>
+                    <img className='justify-self-center rounded-2xl hover:-translate-y-1 hover:scale-110 transition delay-150 duration-300 ease-in-out' src="/img/rec/pokemon-squirtle-squad.gif" alt="imagen squad"/>
+                </div>
+        </div>
+        <div className='bg-amber-100 lg:m-auto m-8 rounded-2xl p-3'>
+            <h2 className='font-bold text-3xl underline'>Foro</h2>
+            <h3 className='font-semibold my-5'>Seccion que permite la comunicacion y expresión entre usuarios. 
+                Esta seccion nos permite poner posts y escribir respuestas a posts de otros usuarios.</h3>
+                <div>
+                    <img className='justify-self-center rounded-2xl hover:-translate-y-1 hover:scale-110 transition delay-150 duration-300 ease-in-out' src="/img/rec/i-will-get-you.gif" alt="imagen squad"/>
+                </div>
+        </div>
+        <div className='bg-amber-100 lg:m-auto m-8 rounded-2xl p-3'>
+            <h2 className='font-bold text-3xl underline'>Otros Usuarios</h2>
+            <h3 className='font-semibold my-5'>Seccion que nos permite la visualizacion de los usuarios que utilizan el portal. 
+                Podremos visualizar los datos de cada usuario y su equipo.
+            </h3>
+            <div>
+                <img className='justify-self-center rounded-2xl hover:-translate-y-1 hover:scale-110 transition delay-150 duration-300 ease-in-out' src="/img/rec/pokemon-pokemon-trainer.gif" alt="imagen trainers"/>
+            </div>
+        </div>
+        <div className='bg-amber-100 lg:m-auto m-8 rounded-2xl p-3'>
+            <h2 className='font-bold text-3xl underline'>Perfil</h2>
+            <h3 className='font-semibold my-5'>Seccion que nos permite la gestion o visualizacion de los datos del usuario.
+                En esta seccion tambien se puede gestionar el equipo personal de criaturas que tenemos.
+            </h3>
+            <div>
+                <img className='justify-self-center h-40 hover:-translate-y-1 hover:scale-130 transition delay-150 duration-300 ease-in-out' src="/img/rec/pokemon-red-red-sprite.gif" alt="imagen red" id="red"/>
+            </div>
+        </div>
+        <ChisteComp
+            chiste={chiste}
+            ContarChiste={ContarChiste}
+        />
+    </main>
+    </>
+  );
+};
+
+export default Home;
