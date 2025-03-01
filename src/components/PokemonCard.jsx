93412a0d1b6cfaa25c1ddc3e9e4760932b4eb996
Lordxavi97 <jaaca97@gmail.com>
2025-03-01 01:44:19 +0100

    Update PokemonCard.jsx

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,9 @@
 
 
 export default function PokemonCard(storage){
-    {storage.map((pokemon) => (
+    storage.map((pokemon) => (
         <div className="p-1 bg-amber-400">
             <h1>{pokemon}</h1>
         </div>
-    ))}
+    ))
 }
