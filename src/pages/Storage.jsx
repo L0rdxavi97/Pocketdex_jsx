fc28922201258c1d91eb6267f4d5fa82dbca4707
Lordxavi97 <jaaca97@gmail.com>
2025-03-01 01:56:36 +0100

    Update Storage.jsx

--- src/pages/Storage.jsx
+++ src/pages/Storage.jsx
@@ -30,7 +30,7 @@ const Storage = ({}) => {
             </div>
             <div>
                 {storage.map((pokemon) => (
-                    <div key={pokemon} className="p-1 bg-amber-400">
+                    <div className="p-1 bg-amber-400">
                         <h1>{pokemon}</h1>
                     </div>
                 ))}
