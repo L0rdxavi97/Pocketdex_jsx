1d9e71c9d739aca30b0dd6ec5ea052a862eda42d
Lordxavi97 <jaaca97@gmail.com>
2025-03-04 09:51:48 +0100

    upd app

--- src/App.jsx
+++ src/App.jsx
@@ -14,26 +14,26 @@ function App() {
     <>
      <Router>
       <div>
-        <header className='relative bg-amber-200 flex lg:flex-row flex-col items-center justify-between py-3 shadow-md inset-shadow-amber-200'>
+        <header className='relative bg-red-500 flex lg:flex-row flex-col items-center justify-between py-3 shadow-md inset-shadow-amber-200'>
           <img className='w-100 mx-1' src="/img/rec/logo.png" alt=""/>
           <nav className='items-center'>
             <ul className='flex flex-row align-middle gap-2 m-2'>
-              <li className='hover:bg-red-400 text-xl p-2 rounded-xl hover:text-white'>
+              <li className='hover:bg-yellow-300 text-white text-xl font-bold p-2 rounded hover:text-black'>
                 <Link to="/">Inicio</Link>
               </li>
-              <li className='hover:bg-red-400 text-xl p-2 rounded-xl hover:text-white'>
+              <li className='hover:bg-yellow-300 text-white text-xl font-bold p-2 rounded hover:text-black'>
                 <Link to="/storage">Almacen</Link>
               </li>
-              <li className='hover:bg-red-400 text-xl p-2 rounded-xl hover:text-white'>
+              <li className='hover:bg-yellow-300 text-white text-xl font-bold p-2 rounded hover:text-black'>
                 <Link to="/forum">Foro</Link>
               </li>
-              <li className='hover:bg-red-400 text-xl p-2 rounded-xl hover:text-white'>
+              <li className='hover:bg-yellow-300 text-white text-xl font-bold p-2 rounded hover:text-black'>
                 <Link to="/users">Usuarios</Link>
               </li>
-              <li className='hover:bg-red-400 text-xl p-2 rounded-xl hover:text-white'>
+              <li className='hover:bg-yellow-300 text-white text-xl font-bold p-2 rounded hover:text-black'>
                 <Link to="/profile">Perfil</Link>
               </li>
-              <li className='hover:bg-red-400 text-xl p-2 rounded-xl hover:text-white'>
+              <li className='hover:bg-yellow-300 text-white text-xl font-bold p-2 rounded hover:text-black'>
                 <Link to="/exit">Salir</Link>
               </li>
             </ul>
