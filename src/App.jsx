35b87449567c436f4dc7975895470c48d1f5fd8a
Lordxavi97 <jaaca97@gmail.com>
2025-03-04 13:40:33 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
     <>
      <Router>
       <div>
-        <header className='relative bg-red-500 flex lg:flex-row flex-col items-center justify-between py-3 shadow-md inset-shadow-amber-200'>
-          <img className='w-100 mx-1' src="/img/rec/logo.png" alt=""/>
+        <header className='relative bg-cyan-700 flex lg:flex-row flex-col items-center justify-between py-3 shadow-md inset-shadow-amber-200'>
+          <Link to="/"><img className='w-100 mx-1 bg-cyan-600 bg rounded-2xl shadow-[0_0_15px_6px_rgba(0,200,200,0.5)]' src="/img/rec/logo.png" alt=""/></Link>
           <nav className='items-center'>
             <ul className='flex flex-row align-middle gap-2 m-2'>
               <li className='hover:bg-yellow-300 text-white text-xl font-bold p-2 rounded hover:text-black'>
