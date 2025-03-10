4f4281f74db72e8543cf71e813f0b8c0499d94fa
Lordxavi97 <jaaca97@gmail.com>
2025-03-10 01:26:57 +0100

    create db tipos

---
+++ src/data/tipos.js
@@ -0,0 +1,21 @@
+export const tipos = [
+    { name: "Normal", color: "bg-yellow-500" },
+    { name: "Fire", color: "bg-red-500" },
+    { name: "Water", color: "bg-blue-500" },
+    { name: "Electric", color: "bg-yellow-300" },
+    { name: "Grass", color: "bg-green-500" },
+    { name: "Ice", color: "bg-cyan-300" },
+    { name: "Fighting", color: "bg-red-700" },
+    { name: "Poison", color: "bg-purple-500" },
+    { name: "Ground", color: "bg-yellow-600" },
+    { name: "Flying", color: "bg-indigo-400" },
+    { name: "Psychic", color: "bg-pink-500" },
+    { name: "Bug", color: "bg-lime-500" },
+    { name: "Rock", color: "bg-yellow-700" },
+    { name: "Ghost", color: "bg-purple-700" },
+    { name: "Dragon", color: "bg-indigo-700" },
+    { name: "Dark", color: "bg-gray-700" },
+    { name: "Steel", color: "bg-gray-400" },
+    { name: "Fairy", color: "bg-pink-300" },
+];
+  
