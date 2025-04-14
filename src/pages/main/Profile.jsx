3b9bea0be3b2735d9f76bf62c7a2ac6950e70914
Lordxavi97 <jaaca97@gmail.com>
2025-04-14 16:24:33 +0200

    los comentarios ya estan ocultos, no se ven en el inspeccionar poero si al ver el codigo

--- src/pages/main/Profile.jsx
+++ src/pages/main/Profile.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Profile = () => {
   return (
     <div className="p-4 bg-amber-200 min-h-screen">
-      /* Pokémon tiene la bendición del Vaticano: Sí, lo has leído correctamente. 
+      {/* Pokémon tiene la bendición del Vaticano: Sí, lo has leído correctamente. 
       A principios de los 2000, cuando Pikachu estaba en sus uñas altas cotas de popularidad, algunos grupos religiosos lanzaron ataques contra Pokémon, acusándola de estar relacionado con rituales satánicos. 
-      Por eso, el Vaticano bendijo oficialmente la franquicia. */
+      Por eso, el Vaticano bendijo oficialmente la franquicia. */}
       <div className="text-center flex flex-col justify-center items-center gap-2">
         <h1 className="text-2xl font-bold">Mi Perfil</h1>
         <img
